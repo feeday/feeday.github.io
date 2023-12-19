f4387a9a7bbb7ab256336f735b37a8201880faec
Puck <127417064+feeday@users.noreply.github.com>
2023-12-19 22:07:40 +0800

    Update detect_yolov5.py

--- py/detect_yolov5.py
+++ py/detect_yolov5.py
@@ -1,8 +1,10 @@
 # YOLOv5 🚀 by Ultralytics, AGPL-3.0 license
 """
 Run YOLOv5 detection inference on images, videos, directories, globs, YouTube, webcam, streams, etc.
+https://www.anaconda.com/download-success
 https://github.com/ultralytics/yolov5
 https://codeload.github.com/ultralytics/yolov5/zip/refs/heads/master
+(base) PS F:\yolov5> pip install yolov5
 Usage - sources:
     $ python detect.py --weights yolov5s.pt --source 0                               # webcam
                                                      img.jpg                         # image
